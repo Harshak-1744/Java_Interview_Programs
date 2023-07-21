a2058f4a441c078cafd5badb0a1a4d9fbb5cae18
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-07-21 18:50:11 +0530

    new file:   Leap_year.java

---
+++ Leap_year.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Leap_year {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number % 4 == 0) 
+        {
+            if (number % 100 == 0) 
+            {
+                if (number % 400 == 0) 
+                {
+                    System.out.println("yes");
+                } else 
+                {
+                    System.out.println("no");
+                }
+            } else 
+            {
+                System.out.println("yes");
+            }
+        } else 
+        {
+            System.out.println("no");
+        }
+
+        sc.close();
+    }
+}
