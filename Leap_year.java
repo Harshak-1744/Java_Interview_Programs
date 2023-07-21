182b08ae8637cfb58fc930cb09207bd4dce4ac2a
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-07-21 18:57:23 +0530

    modified:   .vscode/launch.json 	modified:   Leap_year.java

--- Leap_year.java
+++ Leap_year.java
@@ -4,7 +4,9 @@ public class Leap_year {
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter a year:- ");
         int number = sc.nextInt();
+       
 
         if (number % 4 == 0) 
         {
